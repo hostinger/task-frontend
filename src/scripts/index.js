9775aaba8e5dbb880714e2cef4a49bc9fa6634c6
Arnas <1arnasdickus1@gmail.com>
2019-01-30 15:24:51 +0200

    responsive progress

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,11 @@
 import style from "../_scss/main.scss";
 
 import CalendarController from "./CalendarController";
+import MobileMenu   from "./MobileMenu";
 
 
 const calendarController = new CalendarController;
-
+const mobileMenu = new MobileMenu;
 
 
 

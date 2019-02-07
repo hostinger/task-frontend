a97b883f3a4b6eb868949d40801817dfb5c9c0c6
viktorijama <viktorija.maciune@gmail.com>
2019-02-07 19:11:41 +0200

    Changed links to #;

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -14,7 +14,7 @@ class Header extends Component {
         return (
             <header className="container header-container">
                 <div>
-                    <a href="/" title="Logo">
+                    <a href="#" title="Logo">
                         <img  className="logo" src="./img/logo.png" alt="Logo"/> 
                     </a>
                 </div>

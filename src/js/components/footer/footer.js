5f5c3a502ee6d2f0122e68ef50ae035f6c7f4d95
viktorijama <viktorija.maciune@gmail.com>
2019-02-07 18:00:30 +0200

    Ass maping in footer and menu;

--- src/js/components/footer/footer.js
+++ src/js/components/footer/footer.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
 
+const list = {
+    "data" : [
+      {
+        "name": "Guaranteed 99.9% Uptime"
+      },
+      {
+        "name": "Superior Speed Performance"
+      },
+      {
+        "name": "24/7 Support Chat"
+      },
+      {
+        "name": "30-Day Money-Back Guarantee"
+      }
+    ]
+  }
+  
+  const FooterList = (props) => {
+    return (
+      <React.Fragment>
+        {props.items.data.map(item => (
+          <React.Fragment key={item.id}>
+            <li className="list-item">
+                <div className="checkmark"/>
+                <div>{item.name}</div>
+            </li>
+          </React.Fragment>
+        ))}
+      </React.Fragment>
+    )
+  }
+
 class Footer extends Component {
     render () {
         return (
             <footer className="container footer-container">
                 <ul className="footer-list">
-                    <li className="list-item">
-                        <div className="checkmark"/>
-                        <div>Guaranteed 99.9% Uptime </div>     
-                    </li>
-                    <li className="list-item">
-                        <div className="checkmark"/>
-                        <div>Superior Speed Performance</div>
-                    </li>
-                    <li className="list-item">
-                        <div className="checkmark"/>
-                        <div>24/7 Support Chat</div>
-                    </li>
-                    <li className="list-item">
-                        <div className="checkmark"/>
-                        <div>30-Day Money-Back Guarantee</div>
-                    </li>
+                    <FooterList items={list} />
                 </ul>
             </footer>
         );
--- src/js/components/header/menu/menu.js
+++ src/js/components/header/menu/menu.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react';
 import OnSale from '../onsaleLabel/onsaleLabel.js'
 
+const list = {
+    "data" : [
+      {
+        "name": "Web Hosting"
+      },
+      {
+        "name": "Cloud Hosting"
+      },
+      {
+        "name": "VPS Hosting"
+      },
+      {
+        "name": "Domain Checker"
+      },
+      {
+        "name": "Website Builder"
+      }
+    ]
+  }
+  
+  const MenuList = (props) => {
+    return (
+      <React.Fragment>
+        {props.items.data.map(item => (
+          <React.Fragment key={item.id}>
+            <li>
+                <a href="/" title={item.name} className="menu-link">
+                    {item.name}
+                </a>
+            </li>
+          </React.Fragment>
+        ))}
+      </React.Fragment>
+    )
+  };
+
 class Menu extends Component {
 
     render () {
         return (
             <nav className="menu-container">
                <ul id="menu" className="menu">
-                   <li>
-                       <a href="/" title="Web Hosting" className="menu-link">
-                            Web Hosting
-                       </a>
-                   </li>
-                   <li>
-                       <OnSale />
-                       <a href="/" title="Cloud Hosting" className="menu-link">
-                           Cloud Hosting
-                       </a>
-                   </li>
-                   <li>
-                       <a href="/" title="VPS Hosting" className="menu-link">
-                           VPS Hosting
-                       </a>
-                   </li>
-                   <li>
-                       <a href="/" title="Domain Checker" className="menu-link">
-                           Domain Checker
-                       </a>
-                   </li>
-                   <li>
-                       <a href="/" title="Website Builder" className="menu-link">
-                           Website Builder
-                       </a>
-                   </li>
+                    <MenuList items={list} />
+                    <OnSale />
                </ul>
             </nav>  
     

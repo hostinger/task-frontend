497366b118a01501afbdf6a87cb56ae6ae98c149
unknown <lukutim@gmail.com>
2019-02-16 22:02:25 +0200

    refactored

--- app/components/Featured/discount.js
+++ app/components/Featured/discount.js
@@ -4,7 +4,6 @@ const Discount = () => (
   <div id="spacing">
     <div className="discount_box">save 82%</div>
     <div className="triangle" />
-
     <div className="price">
       <div id="currency">$</div> <div id="big_number">1</div>
       <div>

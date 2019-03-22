46fc7bde93cf0bf79303871a638f6272d1813d82
PovilasPM <povilaspaliukas@gmail.com>
2019-03-22 13:35:30 +0200

    Countdown deadline message and few other minor changes

---
+++ README.md
@@ -0,0 +1,11 @@
+Frontend Developer Task
+
+Small Update
+Zero countdown message - to see changes set different date/time parameters in countDownDate variable in the countdown.js (or main.js) file (before and after countdown deadline).
+Few other fixes were made as well.
+
+LINK
+
+http://puslapiudizainas.lt/hostinger-task/
+
+
---
+++ src/js/nav-modal.js
@@ -0,0 +1,53 @@
+function openModal () {
+    document.getElementById("modal").style.display = "flex";
+    document.body.style.overflow = "hidden";
+    
+}
+
+function closeModal () {
+    document.getElementById("modal").style.display = "none";
+    document.body.style.overflow = "visible";
+}
+
+
+function box1 () {
+    document.getElementById("box1").style.display = "block";
+}
+
+function box1None () {
+    document.getElementById("box1").style.display = "none";
+
+}
+function box2 () {
+    document.getElementById("box2").style.display = "block";
+}
+
+function box2None () {
+    document.getElementById("box2").style.display = "none";
+
+}
+
+function box3 () {
+    document.getElementById("box3").style.display = "block";
+}
+
+function box3None () {
+    document.getElementById("box3").style.display = "none";
+
+}
+function box4 () {
+    document.getElementById("box4").style.display = "block";
+}
+
+function box4None () {
+    document.getElementById("box4").style.display = "none";
+
+}
+function box5 () {
+    document.getElementById("box5").style.display = "block";
+}
+
+function box5None () {
+    document.getElementById("box5").style.display = "none";
+
+}

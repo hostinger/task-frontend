9ff45056db389206aa063713dbc02a11ae6460ce
edvinasba <edvinas.balciuanss@gmail.com>
2019-04-07 15:17:47 +0300

    Responsive nav

---
+++ project/assets/scripts/MenuToggle.js
@@ -0,0 +1,7 @@
+export function menuToggle() {
+    const trigger = document.querySelector('.nav-main__toggle');
+    const menu = document.querySelector('.nav-main__links');
+    trigger.addEventListener('click', () => {
+        menu.classList.toggle('is-active');
+    })
+}

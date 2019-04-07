dbc070f13d19c663e63aadac95667794a5c22aa6
edvinasba <edvinas.balciuanss@gmail.com>
2019-04-07 14:33:27 +0300

    Working countdown & Compressed the image

---
+++ project/assets/scripts/Coutndown.js
@@ -0,0 +1,70 @@
+
+/**
+ * Counts down between current and future times
+ */
+export class CountDown {
+
+  /**
+   * @param {Date in the future to countdown to, format, e.g.: 2019-05-01T15:00:00.000Z}futureDate
+   * @param {Date countdown container selector, e.g.: .banner-ny__offer-timer} selector
+   */
+  constructor(futureDate, selector) {
+    this.selector = selector;
+
+    if (!document.querySelector(this.selector)) {
+      console.error("Invalid container selector");
+      return;
+    }
+
+    // Convert date to UTC
+    if (!futureDate.includes("Z")) {
+      futureDate += "Z";
+    }
+
+    this.futureDate = new Date(futureDate);
+
+    this.startCountDown();
+  }
+
+  /**
+   * Starts the countdown
+   */
+  startCountDown() {
+    const daysContainer = document.querySelector(`${this.selector} .countDown-days`);
+    const hoursContainer = document.querySelector(`${this.selector} .countDown-hours`);
+    const minutesContainer = document.querySelector(`${this.selector} .countDown-minutes`);
+    const secondsContainer = document.querySelector(`${this.selector} .countDown-seconds`);
+
+    const startCountDown = () => {
+      const currentDate = new Date();
+
+      const distance = this.futureDate - currentDate;
+
+      console.log(currentDate.toISOString());
+      console.log(this.futureDate.toISOString());
+      if (distance < 0) {
+        return;
+      }
+
+      const countSecond = 1000;
+      const countMinute = countSecond * 60;
+      const countHour = countMinute * 60;
+      const countDay = countHour * 24;
+
+      const daysLeft =    Math.floor(distance / countDay);
+      const hoursLeft =   Math.floor((distance % countDay) / countHour);
+      const minutesLeft = Math.floor((distance % countHour) / countMinute);
+      const secondsLeft = Math.floor((distance % countMinute) / countSecond);
+
+
+      daysContainer.textContent = daysLeft;
+      hoursContainer.textContent = hoursLeft;
+      minutesContainer.textContent = minutesLeft;
+      secondsContainer.textContent = secondsLeft;
+
+      setTimeout(startCountDown, 1000);
+    }
+
+    setTimeout(startCountDown, 1000);
+  }
+}
--- project/assets/scripts/main.js
+++ project/assets/scripts/main.js
@@ -1,1 +1,9 @@
-import './../styles/main.scss'
+import './otherImports';
+import { CountDown } from "./Coutndown";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nyCounter = new CountDown(
+        "2019-05-01T15:00:00.000Z",
+        ".banner-ny__offer-timer"
+      );
+})

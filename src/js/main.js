25f62f6aa069b6a221d4ec1d1f3ac410cff2c84c
Raulis <raulis.radziukas@gmail.com>
2019-04-10 21:30:54 +0300

    add countdown timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,32 @@
+////////////////////////
+//////Countdown timer
+////////////////////////
+const countDownDate = new Date();
+const daysToAdd = 3;
+countDownDate.setDate(countDownDate.getDate() + daysToAdd);
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const timer = setInterval(function() {
+    // Get todays date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate.getTime() - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    days.innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24));
+    hours.innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes.innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    seconds.innerHTML = Math.floor((distance % (1000 * 60)) / 1000);
+}, 1000);
+
+///////////////////////////
+//Mobile navigation interactivity
+///////////////////////////
 const mobileNav = document.querySelector('.header__mobile-nav-holder');
 const mobileNavButton = document.querySelector('.header__burger-cont');
 const mobileX = document.querySelector('.x-button');

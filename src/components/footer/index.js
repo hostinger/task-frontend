877d5da7184da92037ff6ce0b408de8f0ec37829
Chateux1 <m.satas@gmail.com>
2019-05-17 15:59:21 +0300

    Setup footer
    Add text of elements
    Add tick icons from font awesome
    
    Changes:
    - add/change global colors
    - add more fonts similar to Calibri (mac problem)

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,24 @@ const Footer = () => (
   <footer className="page-footer">
     <div className="container">
       <div className="content-wrapper">
-        <p>Footer</p>
+        <div className="footer">
+          <a>
+            <i class="fas fa-check" />
+            <span> Guaranteed 99.9% Uptime</span>
+          </a>
+          <a>
+            <i class="fas fa-check" />
+            <span> Superior Speed Performance</span>
+          </a>
+          <a>
+            <i class="fas fa-check" />
+            <span> 24/7 Support Chat</span>
+          </a>
+          <a>
+            <i class="fas fa-check" />
+            <span> 30-Day Money-Back Guarantee</span>
+          </a>
+        </div>
       </div>
     </div>
   </footer>

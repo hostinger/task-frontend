9c418f1a2e3145d165a3a0da52b106b42eb4672f
Chateux1 <m.satas@gmail.com>
2019-05-19 12:52:23 +0300

    Make footer responsive
    make footer responsive using css grid
    rewrite classnames to be included from footer.scss
    fix icons to use className instead of class
    use for loop to create classes for each menu item
    add 3 responsive options:
    - items in 1 row
    - items in 2 rows
    - items in 1 column

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,28 +1,27 @@
 import React from "react";
+import styles from "../../styles/components/footer.scss";
 
 const Footer = () => (
   <footer className="page-footer">
-    <div className="container">
-      <div className="content-wrapper">
-        <div className="footer">
-          <a>
-            <i class="fas fa-check" />
-            <span> Guaranteed 99.9% Uptime</span>
-          </a>
-          <a>
-            <i class="fas fa-check" />
-            <span> Superior Speed Performance</span>
-          </a>
-          <a>
-            <i class="fas fa-check" />
-            <span> 24/7 Support Chat</span>
-          </a>
-          <a>
-            <i class="fas fa-check" />
-            <span> 30-Day Money-Back Guarantee</span>
-          </a>
-        </div>
-      </div>
+    {/* <div className="container"> */}
+    <div className={styles.contentFooter}>
+      <a className={styles.menuItem1}>
+        <i className="fas fa-check" />
+        <span> Guaranteed 99.9% Uptime</span>
+      </a>
+      <a className={styles.menuItem2}>
+        <i className="fas fa-check" />
+        <span> Superior Speed Performance</span>
+      </a>
+      <a className={styles.menuItem3}>
+        <i className="fas fa-check" />
+        <span> 24/7 Support Chat</span>
+      </a>
+      <a className={styles.menuItem4}>
+        <i className="fas fa-check" />
+        <span> 30-Day Money-Back Guarantee</span>
+      </a>
+      {/* </div> */}
     </div>
   </footer>
 );

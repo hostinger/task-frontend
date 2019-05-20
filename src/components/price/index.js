68f2319992e24c0682beadba6cf90337718a3c28
Chateux1 <m.satas@gmail.com>
2019-05-20 23:12:54 +0300

    Refactor price component
    Add id tags
    Move font size styling to global file

--- src/components/price/index.js
+++ src/components/price/index.js
@@ -15,10 +15,18 @@ class Price extends React.Component {
               save 82%
             </div>
           </div>
-          <div className={styles.price__currency}>$</div>
-          <div className={styles.price__dollar}>1</div>
-          <div className={styles.price__cents}>45</div>
-          <div className={styles.price__month}>/mo</div>
+          <div className={styles.price__currency} id="price__currency">
+            $
+          </div>
+          <div className={styles.price__dollar} id="price__dollar">
+            1
+          </div>
+          <div className={styles.price__cents} id="price__cents">
+            45
+          </div>
+          <div className={styles.price__month} id="price__month">
+            /mo
+          </div>
         </div>
       </aside>
     );

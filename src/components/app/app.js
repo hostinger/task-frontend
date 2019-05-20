d22583c19ffc09b826d38d716a11ea51d070c0cb
Chateux1 <m.satas@gmail.com>
2019-05-20 10:55:32 +0300

    Fix background image
    Flip the image horizontally to comply with design
    Darken the image

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,16 +4,17 @@ import Footer from "../footer";
 import Offer from "../offer";
 
 import styles from "./app.scss";
-// import style from "../../index.scss";
 
 class App extends Component {
   render() {
     return (
       <div className={styles.container__background}>
-        <div className={styles.container__content}>
-          <Header />
-          <Offer />
-          <Footer />
+        <div className={styles.container__darken}>
+          <div className={styles.container__content}>
+            <Header />
+            <Offer />
+            <Footer />
+          </div>
         </div>
       </div>
     );

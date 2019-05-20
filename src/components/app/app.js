c0a2e9ab711692c9b866aa082224d3a4fe4dddc3
Chateux1 <m.satas@gmail.com>
2019-05-20 11:48:33 +0300

    Add price component
    Create price component which displays price aside
    Add dialog box made in css (generated with online tool)
    Design price component with css grid
    
    Fixes:
    Rewrite some css to use sass nesting
    Change offer component to float left

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "../header";
 import Footer from "../footer";
 import Offer from "../offer";
+import Price from "../price";
 
 import styles from "./app.scss";
 
@@ -13,6 +14,7 @@ class App extends Component {
           <div className={styles.container__content}>
             <Header />
             <Offer />
+            <Price />
             <Footer />
           </div>
         </div>
---
+++ src/components/price/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../../styles/components/price.scss";
+
+class Price extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <aside>
+        <div className={styles.price__container}>
+          <div className={styles.price__dialog}>
+            <div className={styles.arrow_box} id="arrow-box">
+              save 82%
+            </div>
+          </div>
+          <div className={styles.price__currency}>$</div>
+          <div className={styles.price__dollar}>1</div>
+          <div className={styles.price__cents}>45</div>
+          <div className={styles.price__month}>/mo</div>
+        </div>
+      </aside>
+    );
+  }
+}
+
+export default Price;

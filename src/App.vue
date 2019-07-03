078a8edebedc13aa4c96be2188dbc0599eb3d13e
Domas <domas.briedis@gmail.com>
2019-07-03 21:23:21 +0300

    Add scss vars and background

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,14 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  height: 100vh;
+  margin: 0px;
+}
 #app {
-
+  background-color: $white;
+  background-image: url('assets/background.jpg');
+  background-size: cover;
+  height: 100vh;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/styles/_vars.scss";`
+      }
+    }
+  }
 }

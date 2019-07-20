e21c3fb735b92f8a8343cd0cf2c19321355e2776
Gedridze <edvinas.kilciauskas@gmail.com>
2019-07-20 17:38:05 +0300

    Create offer text and counter

--- src/components/BannerOffer.vue
+++ src/components/BannerOffer.vue
@@ -1,5 +1,29 @@
 <template>
-    
+
+    <div class="offer">
+        <div class="offer-text">
+            New Year Offer <br><em>82% Off</em> Web Hosting
+        </div>
+        <div class="offer-timer">
+            <span class="arrow-right"></span>
+            <div class="timer">
+                <div class="time">
+                    <div class="count">00<br><span>Days</span></div>
+                </div>
+                <div class="time">
+                    <div class="count">00<br><span>Hours</span></div>
+                </div>
+                <div class="time">
+                    <div class="count">00<br><span>Minutes</span></div>
+                </div>
+                <div class="time">
+                    <div class="count">00<br><span>Seconds</span></div>
+                </div>
+            </div>
+            <span class="arrow-left"></span>
+        </div>
+        <button class="offer-button">Get Started Now</button>
+    </div>
 </template>
 
 <script>
@@ -8,6 +32,6 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    @import "../assets/styles/components/bannerOffer";
 </style>
--- src/views/BannerPage.vue
+++ src/views/BannerPage.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container main-container">
         <div class="row h-100">
-            <div class="col-lg-6 col-12 order-2 order-lg-1" style="border: 1px solid red">
-
+            <div class="col-lg-6 col-12 order-2 order-lg-1">
+                <banner-offer/>
             </div>
-            <div class="col-lg-6 col-12 order-1 order-lg-2" style="border: 1px solid red">
+            <div class="col-lg-6 col-12 order-1 order-lg-2">
                 <banner-price/>
             </div>
         </div>
@@ -14,9 +14,10 @@
 <script>
 
     import BannerPrice from '../components/BannerPrice'
+    import BannerOffer from '../components/BannerOffer'
     export default {
         name: "BannerPage",
-        components: {BannerPrice}
+        components: {BannerPrice, BannerOffer}
     }
 </script>
 

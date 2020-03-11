1c5ecda38ebaf8564018c23d7d9a8fec90332bdb
Ruta Sipkauskaite <ruta.sipkauskaite@gmail.com>
2020-03-11 02:11:05 +0200

    Finished task

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,52 @@
+<template>
+    <span>
+        <v-navigation-drawer app v-model="drawer" light disable-resize-watcher right>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-item :key="index">
+                        <v-list-item-content>{{item.title}}</v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar app color="transparent" class="app-bar" dark flat>
+            <v-img :src="appLogo" max-width="170" />
+            <v-spacer></v-spacer>
+            <v-app-bar-nav-icon class="hidden-lg-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-card-text class="hidden-md-and-down menu-items">
+                <template v-for="(item, index) in items">
+                    <v-btn text :key="index">{{item.title}}</v-btn>
+                </template>
+            </v-card-text>
+        </v-app-bar>
+    </span>
+</template>
+
+<script>
+import logo from '@/assets/logo.png';
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appLogo: logo,
+            drawer: false,
+            items: [
+                { title: 'Web Hosting' },
+                { title: 'Cloud Hosting' },
+                { title: 'VPS Hosting' },
+                { title: 'Domain Checker' },
+                { title: 'Website Builder' }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss">
+.app-bar {
+    padding: 10px 30px;
+}
+.menu-items {
+    max-width: fit-content;
+}
+</style>
---
+++ src/components/HomeHero.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container
+        fluid
+        fill-width
+        fill-height
+        absolute
+        class="home-hero"
+        :style="{ backgroundImage: `url(${heroImg})` }"
+    >
+        <v-layout justify-center align-center>
+            <v-row align="center" justify="center">
+                <v-col sm="6">
+                    <div
+                        class="display-2 font-weight-black white--text text-xs-center"
+                    >New Year Offer</div>
+                    <div
+                        class="display-2 font-weight-black white--text text-xs-center mb-3"
+                    >82% OFF Web Hosting</div>
+                    <div
+                        class="display-1 font-weight-bold white--text text-xs-center"
+                    >days hours minutes seconds</div>
+                    <v-btn class="mt-5 red">
+                        <v-icon large color="white">Get Started Now</v-icon>
+                    </v-btn>
+                </v-col>
+                <v-col sm="6">
+                    <div class="display-4 font-weight-black white--text text-xs-center">$ 1 45</div>
+                </v-col>
+            </v-row>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+import heroImg from '@/assets/background.jpg';
+export default {
+    name: 'HomeHero',
+    data() {
+        return {
+            heroImg
+        };
+    }
+};
+</script>
+
+<style lang="scss">
+.home-hero {
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
